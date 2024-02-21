39b1de4f64f794460f40eb87bc61840071de2127
Mayank-Admin <mayank-admin@MacBook-Pro-von-Mayank.local>
2024-02-21 12:57:13 +0100

    Boostrap layout integrated

---
+++ src/layouts/backend/MainLayout.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
+import "../../assets/admin/css/styles.css";
+import "../../assets/admin/js/scripts.js";
+
+const MainLayout = () => {
+  return (
+    <div className="sb-nav-fixed">
+      <Navbar />
+      <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+          <Sidebar />
+        </div>
+        <div id="layoutSidenav_content">
+          <main>Welcome</main>
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
