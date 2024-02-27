573d7a7468aa347d5b5373b5fd3171b9b673c2bb
Mayank-Admin <mayank-admin@MacBook-Pro-von-Mayank.local>
2024-02-27 13:28:28 +0100

    Added axios interceptors for unauthenticated users

--- src/AdminPrivateRoute.js
+++ src/AdminPrivateRoute.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import axios from "axios";
+import swal from "sweetalert";
 
 const AdminPrivateRoute = () => {
+  const navigate = useNavigate();
   const [authenticated, setAuthenticated] = useState(false);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    // Check if the user is authenticated
     axios
       .get("/api/checkingAuthenticated")
       .then((response) => {
@@ -15,8 +18,29 @@ const AdminPrivateRoute = () => {
         }
         setLoading(false);
       })
-      .catch(() => setLoading(false));
-  }, []);
+      .catch(() => {
+        setLoading(false);
+      });
+
+    axios.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response.status === 401) {
+          swal({
+            text: "You are not authorized to access this page.",
+            icon: "warning",
+            button: false,
+            timer: 2000,
+          }).then(() => {
+            navigate("/login");
+          });
+        }
+        return Promise.resolve(error);
+      }
+    );
+
+    // Cleanup the interceptor when the component unmounts
+  }, [navigate]);
 
   if (loading) return <h1>Loading...</h1>;
 
