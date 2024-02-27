0bc5e3fdea419601c177d907e7a8d817c95aa988
Mayank-Admin <mayank-admin@MacBook-Pro-von-Mayank.local>
2024-02-27 11:55:09 +0100

    Protected routing to admin dashboard for authenticated users

--- src/AdminPrivateRoute.js
+++ src/AdminPrivateRoute.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import MainLayout from "../src/layouts/backend/MainLayout";
+import axios from "axios";
 
 const AdminPrivateRoute = () => {
-  const isAuthenticated = localStorage.getItem("auth_token");
+  const [authenticated, setAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get(`/api/checkingAuthenticated`).then((res) => {
+      if (res.status === 200) {
+        setAuthenticated(true);
+      }
+      setLoading(false);
+      return () => {
+        setAuthenticated(false);
+      };
+    });
+  }, []);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  const isAuthenticated = authenticated;
 
   return isAuthenticated ? (
     <MainLayout>
